3c80be55dd6ee5a5745d861779c7a6086d2a3c36
Juan Gonzalo Castano Rios <juan.castano@mercadolibre.com.co>
2024-10-28 11:16:48 -0500

    se adicionan estilos a la lista de albumes

--- app/src/main/java/com/vinylsmobile/ui/fragments/AlbumFragment.kt
+++ app/src/main/java/com/vinylsmobile/ui/fragments/AlbumFragment.kt
@@ -30,7 +30,7 @@ class AlbumFragment : Fragment() {
         binding = FragmentAlbumBinding.inflate(inflater, container, false)
 
 
-        val api = Retrofit.Builder().baseUrl("http://localhost:3000/")
+        val api = Retrofit.Builder().baseUrl("https://backvynils-q6yc.onrender.com/")
             .addConverterFactory(GsonConverterFactory.create()).build().create(AlbumApi::class.java)
 
         val repository = AlbumRepository(api)
@@ -38,7 +38,7 @@ class AlbumFragment : Fragment() {
             this, AlbumViewModelFactory(repository)
         ).get(AlbumViewModel::class.java)
 
-        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL, false)
 
         viewModel.albums.observe(viewLifecycleOwner, Observer { albums ->
             binding.recyclerView.adapter = AlbumAdapter(albums)
