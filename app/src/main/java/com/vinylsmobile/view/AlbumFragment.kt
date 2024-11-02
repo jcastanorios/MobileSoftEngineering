754b6edafd119e46fac9d9308aadce359a283b16
Juan Gonzalo Castano Rios <juan.castano@mercadolibre.com.co>
2024-11-02 08:28:47 -0500

    se adiciona navegación entre ativities

--- app/src/main/java/com/vinylsmobile/view/AlbumFragment.kt
+++ app/src/main/java/com/vinylsmobile/view/AlbumFragment.kt
@@ -39,7 +39,7 @@ class AlbumFragment : Fragment() {
         }
 
 
-        binding.albumListTitle.setOnClickListener {
+        binding.albumForwardButton.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, AlbumListFragment())
                 .addToBackStack(null)
