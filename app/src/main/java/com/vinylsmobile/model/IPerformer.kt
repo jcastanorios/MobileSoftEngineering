d7867f8f8105c127535933728df8c6db15a84371
Santiago Alvarez Soto <alvarezsoto13@gmail.com>
2024-11-13 23:07:12 -0500

    Agregar tipo a la interfaz IPerformer

--- app/src/main/java/com/vinylsmobile/model/IPerformer.kt
+++ app/src/main/java/com/vinylsmobile/model/IPerformer.kt
@@ -1,8 +1,14 @@
 package com.vinylsmobile.model
 
+enum class PerformerType {
+    ARTIST,
+    BAND
+}
+
 interface IPerformer {
     val id: Int
     val name: String
     val image: String
     val description: String
+    val type: PerformerType
 }
