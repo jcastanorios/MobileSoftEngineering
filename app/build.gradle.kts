8c4d844c4461483fa56d490442ea6e57c78d73d2
Orlando Giovanny Solarte Delgado <o.solarte@uniandes.edu.co>
2024-11-14 19:07:48 -0500

    Avances en la configuracion Room + Dao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
+
 
 android {
     namespace = "com.vinylsmobile"
@@ -84,4 +86,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Dependencias de Room
+    implementation("androidx.room:room-runtime:2.6.0")
+    kapt("androidx.room:room-compiler:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
+
 }
