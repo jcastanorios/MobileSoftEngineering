0970f3aa1729a156fa57c42b01039748225ba0a7
Santiago Alvarez Soto <s.alvarez112@uniandes.edu.co>
2024-11-21 20:46:07 -0500

    Flujo básico considerando el rol del usuario (#126)

--- app/src/main/java/com/vinylsmobile/view/CollectionActivity.kt
+++ app/src/main/java/com/vinylsmobile/view/CollectionActivity.kt
@@ -1,8 +1,13 @@
 package com.vinylsmobile.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import com.vinylsmobile.Application
 import com.vinylsmobile.R
+import com.vinylsmobile.UserRole
 
 class CollectionActivity : AppCompatActivity() {
 
@@ -15,5 +20,35 @@ class CollectionActivity : AppCompatActivity() {
                 .replace(R.id.fragment_container, CollectionFragment())
                 .commit()
         }
+
+        val fab: View = findViewById(R.id.fab_collection_activity)
+
+        val app = application as Application
+        val userRole = app.getUserRole()
+        if (userRole == UserRole.VISITOR) {
+            fab.visibility = View.GONE
+        } else {
+            fab.visibility = View.VISIBLE
+            fab.setOnClickListener { view ->
+                showPopupMenu(view)
+            }
+        }
+    }
+
+    private fun showPopupMenu(view: View) {
+        val popupMenu = PopupMenu(this, view)
+        popupMenu.inflate(R.menu.menu_fab_collection_activity_options)
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.action_create_album -> {
+                    // Handle settings
+                    val intent = Intent(this, CreateAlbumActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+        popupMenu.show()
     }
 }
